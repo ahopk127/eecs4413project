2cef64c574a6c7b5d22be21e0582c984f249858c
Adrien Hopkins <adrien.p.hopkins@gmail.com>
2025-03-03 21:26:43 -0500

    Add generated Spring project
    Generated from https://start.spring.io/

---
+++ src/main/java/ca/yorku/eecs4413project/Eecs4413projectApplication.java
@@ -0,0 +1,13 @@
+package ca.yorku.eecs4413project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eecs4413projectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Eecs4413projectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ca/yorku/eecs4413project/Eecs4413projectApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.yorku.eecs4413project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Eecs4413projectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
